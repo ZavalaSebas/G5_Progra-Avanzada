32236d1f77d67b4b472244a5ce9878a4672d34e7
JordanHer <jordanpc0101@gmail.com>
2023-10-17 16:50:58 -0600

    Se crea la conexion con la BD y se genera el modelo, se crea el metodo registarusuario en el API

---
+++ API/API/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using API.Entities;
+using Microsoft.Ajax.Utilities;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("RegistrarCuenta")]
+        public string RegistrarCuenta(UsuarioEnt entidad)
+        {
+            using (var context = new ProyectoPrograAvanzadaEntities())
+            {
+                usuarios user = new usuarios();
+                user.Nombre = entidad.Nombre;
+                user.Username = entidad.Username;
+                user.Correo = entidad.Correo;
+                user.Contrasenna = entidad.Contrasenna;
+
+                context.usuarios.Add(user);
+                context.SaveChanges();
+
+                return "Registro exitoso";
+
+            }
+        }
+
+    }
+}
--- API/API/Entities/UsuarioEnt.cs
+++ API/API/Entities/UsuarioEnt.cs
@@ -1,7 +1,13 @@
-﻿namespace G5_ProgramacionAvanzada.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Entities
 {
     public class UsuarioEnt
     {
+
         public int IDUser { get; set; }
         public string Username { get; set; }
         public string Nombre { get; set; }
@@ -9,5 +15,6 @@
         public string Correo { get; set; }
         public string Contrasenna { get; set; }
         public int IDRol { get; set; }
+
     }
 }
