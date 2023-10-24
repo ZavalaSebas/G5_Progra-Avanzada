eba40484018f846fdefaa91af16b79e52485e489
JordanHer <jordanpc0101@gmail.com>
2023-10-24 16:57:10 -0600

    Se agregan algunas entidades en el Proyecto Web

--- G5_ProgramacionAvanzada/G5_ProgramacionAvanzada/Entities/UsuarioEnt.cs
+++ G5_ProgramacionAvanzada/G5_ProgramacionAvanzada/Entities/UsuarioEnt.cs
@@ -1,4 +1,6 @@
-﻿namespace G5_ProgramacionAvanzada.Entities
+﻿using System;
+
+namespace G5_ProgramacionAvanzada.Entities
 {
     public class UsuarioEnt
     {
@@ -9,5 +11,32 @@
         public string Correo { get; set; }
         public string Contrasenna { get; set; }
         public int IDRol { get; set; }
+    }
+
+    public class RolEnt
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        
+    }
+
+    public class ReservaEnt
+    {
+        public int ID { get; set; }
+        public int type { get; set; }
+        public int IDUser { get; set; }
+        public int IDDestino { get; set; }
+        public DateTime salida { get; set; }
+        public DateTime entrada { get; set; }
+
+    }
+
+    public class HotelesEnt
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string pais { get; set; }
+
     }
 }
