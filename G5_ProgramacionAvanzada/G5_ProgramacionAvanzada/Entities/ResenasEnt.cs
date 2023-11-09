6b3f935dff9ae799e0e4aa4e43d365e00048b30c
Sebas Zavala <63665504+ZavalaSebas@users.noreply.github.com>
2023-11-09 10:35:11 -0600

    ResenasWEB

---
+++ G5_ProgramacionAvanzada/G5_ProgramacionAvanzada/Entities/ResenasEnt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace G5_ProgramacionAvanzada.Entities
+{
+    public class ResenaEnt
+    {
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Puntuacion { get; set; }
+        public long? IdUsuario { get; set; }
+        public long? IdHotel { get; set; }
+    }
+}
